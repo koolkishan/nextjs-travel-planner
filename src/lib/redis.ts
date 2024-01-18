ffe9719dd8ea149f065657492df144add4760f01
Kishan Sheth <kishansheth21@gmail.com>
2024-01-19 01:08:23 +0530

    Redis URL Update.

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,9 @@
 import Redis from "ioredis";
 
 // You can get the REDIS_URL from your environment variables
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL =
+  process.env.REDIS_URL ||
+  "redis://localhost:rediss://aptible:dUd2V4fZjmBXqSPAfR8NTKb0uDCaIo7j@db-shared-us-east-1-wat-122849.aptible.in:29679";
 
 const connection = new Redis(REDIS_URL);
 
